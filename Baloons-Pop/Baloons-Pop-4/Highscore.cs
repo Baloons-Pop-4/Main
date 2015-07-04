874df331a0305aa35ce6cdbd28213fec38e5e0bc
KonstantinSimeonov <kon.simeonov@gmail.com>
2015-07-04 19:44:58 +0300

    Started refactoring and reforming
    Changes list:
    
    1. Renamed solution and project
    2. Renamed some of the methods and variables to make them
    self-documenting
    3. Refactored some methods, so they're shorter and more understandable
    3. Started splitting the logic of the program into multiple .cs file
    4. Added a file for interface declaration(Contracts.cs)
    5. Implemented Singletion design pattern for the Engine class
    6. Added basic interface abstraction(ConsoleUI and ILogger), with the
    intent of addingan option GUI

---
+++ Baloons-Pop/Baloons-Pop-4/Highscore.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+
+public struct PlayerScore : IComparable<PlayerScore>
+{
+    public int Value;
+    public string Name;
+    public PlayerScore(int value, string name)
+    {
+
+        Value = value;
+        Name = name;
+    }
+
+    public int CompareTo(PlayerScore other)
+    {
+        return Value.CompareTo(other.Value);
+    }
+}
+
+public class HighScoreUtility
+{
+    public static void sortAndPrintChartFive(string[,] tableToSort)
+    {
+
+        List<PlayerScore> klasirane = new List<PlayerScore>();
+
+        for (int i = 0; i < 5; ++i)
+        {
+            if (tableToSort[i, 0] == null)
+            {
+                break;
+            }
+
+            klasirane.Add(new PlayerScore(int.Parse(tableToSort[i, 0]), tableToSort[i, 1]));
+
+        }
+
+        klasirane.Sort();
+        Console.WriteLine("---------TOP FIVE CHART-----------");
+        for (int i = 0; i < klasirane.Count; ++i)
+        {
+            PlayerScore slot = klasirane[i];
+            Console.WriteLine("{2}.   {0} with {1} moves.", slot.Name, slot.Value, i + 1);
+        }
+        Console.WriteLine("----------------------------------");
+
+
+    }
+
+    public static bool signIfSkilled(string[,] Chart, int points)
+    {
+        bool Skilled = false;
+        int worstMoves = 0;
+        int worstMovesChartPosition = 0;
+        for (int i = 0; i < 5; i++)
+        {
+            if (Chart[i, 0] == null)
+            {
+                Console.WriteLine("Type in your name.");
+                string tempUserName = Console.ReadLine();
+                Chart[i, 0] = points.ToString();
+                Chart[i, 1] = tempUserName;
+                Skilled = true;
+                break;
+            }
+        }
+        if (Skilled == false)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                if (int.Parse(Chart[i, 0]) > worstMoves)
+                {
+                    worstMovesChartPosition = i;
+                    worstMoves = int.Parse(Chart[i, 0]);
+                }
+            }
+        }
+        if (points < worstMoves && Skilled == false)
+        {
+            Console.WriteLine("Type in your name.");
+            string tempUserName = Console.ReadLine();
+            Chart[worstMovesChartPosition, 0] = points.ToString();
+            Chart[worstMovesChartPosition, 1] = tempUserName;
+            Skilled = true;
+        }
+        return Skilled;
+    }
+}
