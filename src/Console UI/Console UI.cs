0d61cbefb052174d71965e0f3a6935d89414bfe6
KonstantinSimeonov <kon.simeonov@gmail.com>
2015-08-11 23:05:45 +0300

    Refactored the PrintField method in the ConsoleUI

--- src/Console UI/Console UI.cs
+++ src/Console UI/Console UI.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.Text;
 
 using Contracts;
 
 public class ConsoleUI : IBaloonsUserInterface
 {
+    private const string COLUMNS_INDECES = "    0 1 2 3 4 5 6 7 8 9 ";
+    private const string EMPTY_SPOT = "  ";
+
     public void PrintMessage(string message)
     {
         Console.WriteLine(message);
@@ -11,33 +15,35 @@ public class ConsoleUI : IBaloonsUserInterface
 
     public void PrintField(byte[,] matrix)
     {
-        Console.Write("    ");
-        for (byte column = 0; column < matrix.GetLongLength(1); column++)
-        {
-            Console.Write(column + " ");
-        }
+        var result = new StringBuilder();
 
-        Console.Write(Environment.NewLine + "   " + new string('-', matrix.GetLength(1) * 2 + 1) + Environment.NewLine);
+        result.AppendLine(COLUMNS_INDECES);
+        result.AppendLine(this.GetFieldFrame(matrix.GetLength(1)));
 
         for (byte i = 0; i < matrix.GetLongLength(0); i++)
         {
-            Console.Write(i + " | ");
+            result.Append(i + " | ");
             for (byte j = 0; j < matrix.GetLongLength(1); j++)
             {
                 if (matrix[i, j] == 0)
                 {
-                    Console.Write("  ");
-                    continue;
+                    result.Append(EMPTY_SPOT);
+
+                }
+                else
+                {
+                    result.Append(matrix[i, j] + " ");
                 }
 
-                Console.Write(matrix[i, j] + " ");
+
             }
 
-            Console.WriteLine("| ");
+            result.AppendLine("| ");
         }
 
-        Console.Write("   " + new string('-', matrix.GetLength(1) * 2 + 1) + Environment.NewLine);
+        result.Append(this.GetFieldFrame(matrix.GetLength(1)));
 
+        Console.WriteLine(result);
     }
 
     public void PrintHighscore(string highscore)
@@ -49,5 +55,11 @@ public class ConsoleUI : IBaloonsUserInterface
     public string ReadUserInput()
     {
         return Console.ReadLine();
+    }
+
+    private string GetFieldFrame(int repeat)
+    {
+        var fieldFrame = "   " + new string('-', repeat * 2 + 1);
+        return fieldFrame;
     }
 }
