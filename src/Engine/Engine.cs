8b5e1829ddb042319f469de77fbf50393bba9cf7
KonstantinSimeonov <kon.simeonov@gmail.com>
2015-08-11 17:26:13 +0300

    Extracted all messages in const strings

--- src/Engine/Engine.cs
+++ src/Engine/Engine.cs
@@ -4,6 +4,18 @@ using Contracts;
 
 public class Engine : IEngine
 {
+    #region Constants
+    private const string EXIT = "EXIT";
+    private const string TOP = "TOP";
+    private const string RESTART = "RESTART";
+    private const string WRONG_INPUT = "Wrong input! Try Again!";
+    private const string CANNOT_POP_MISSING_BALLOON = "Cannot pop missing ballon!";
+    private const string WIN_MESSAGE_TEMPLATE = "Gratz ! You completed it in {0} moves.";
+    private const string NOT_IN_TOP_FIVE = "I am sorry you are not skillful enough for TopFive chart!";
+    private const string MOVE_PROMPT = "Enter a row and column: ";
+    private const string ON_EXIT_MESSAGE = "Good Bye!";
+    #endregion
+
     // Singleton design pattern
 
     private static Engine instance = new Engine();
@@ -38,21 +50,22 @@ public class Engine : IEngine
         this.UI.PrintField(matrix);
         string temp = null;
         int userMoves = 0;
-        while (temp != "EXIT")
+
+        while (temp != EXIT)
         {
-            this.UI.PrintMessage("Enter a row and column: ");
+            this.UI.PrintMessage(MOVE_PROMPT);
             temp = Console.ReadLine();
             temp = temp.ToUpper().Trim();
 
             switch (temp)
             {
-                case "RESTART":
+                case RESTART:
                     matrix = GameLogic.GenerateField();
                     this.UI.PrintField(matrix);
                     userMoves = 0;
                     break;
 
-                case "TOP":
+                case TOP:
                     HighScoreUtility.sortAndPrintChartFive(topFive);
                     break;
 
@@ -63,27 +76,27 @@ public class Engine : IEngine
                         userRow = int.Parse(temp[0].ToString());
                         if (userRow > 4)
                         {
-                            this.UI.PrintMessage("Wrong input ! Try Again ! ");
+                            this.UI.PrintMessage(WRONG_INPUT);
                             continue;
                         }
                         userColumn = int.Parse(temp[2].ToString());
 
                         if (GameLogic.change(matrix, userRow, userColumn))
                         {
-                            this.UI.PrintMessage("cannot pop missing ballon!");
+                            this.UI.PrintMessage(CANNOT_POP_MISSING_BALLOON);
                             continue;
                         }
                         userMoves++;
                         if (GameLogic.doit(matrix))
                         {
-                            this.UI.PrintMessage(string.Format("Gratz ! You completed it in {0} moves.", userMoves));
+                            this.UI.PrintMessage(string.Format(WIN_MESSAGE_TEMPLATE, userMoves));
                             if (HighScoreUtility.signIfSkilled(topFive, userMoves))
                             {
                                 HighScoreUtility.sortAndPrintChartFive(topFive);
                             }
                             else
                             {
-                                this.UI.PrintMessage("I am sorry you are not skillful enough for TopFive chart!");
+                                this.UI.PrintMessage(NOT_IN_TOP_FIVE);
                             }
                             matrix = GameLogic.GenerateField();
                             userMoves = 0;
@@ -93,13 +106,13 @@ public class Engine : IEngine
                     }
                     else
                     {
-                        this.UI.PrintMessage("Wrong input ! Try Again ! ");
+                        this.UI.PrintMessage(WRONG_INPUT);
                         break;
                     }
 
 
             }
         }
-        Console.WriteLine("Good Bye! ");
+        this.UI.PrintMessage(ON_EXIT_MESSAGE);
     }
 }
