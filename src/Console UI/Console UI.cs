f19df82197be68b39cec2efaa1a8c5bfbab47dbf
KonstantinSimeonov <kon.simeonov@gmail.com>
2015-08-15 09:45:31 +0300

    Added colors and extracted methods from the PrintMatrix method

--- src/Console UI/Console UI.cs
+++ src/Console UI/Console UI.cs
@@ -1,9 +1,24 @@
 ﻿using System;
+using System.Collections.Generic;
 
 using Contracts;
 
 public class ConsoleUI : IBaloonsUserInterface
 {
+    private const string COLUMN_INDECES = "    0 1 2 3 4 5 6 7 8 9 ";
+    private const ConsoleColor DEFAULT_COLOR = ConsoleColor.White;
+
+    private readonly Dictionary<int, ConsoleColor> colorsByNumbers = new Dictionary<int, ConsoleColor>();
+
+    public ConsoleUI()
+    {
+        this.colorsByNumbers.Add(1, ConsoleColor.Red);
+        this.colorsByNumbers.Add(2, ConsoleColor.Green);
+        this.colorsByNumbers.Add(3, ConsoleColor.Blue);
+        this.colorsByNumbers.Add(4, ConsoleColor.Yellow);
+        this.SetConsoleColorToDefault();
+    }
+
     public void PrintMessage(string message)
     {
         Console.WriteLine(message);
@@ -11,17 +26,19 @@ public class ConsoleUI : IBaloonsUserInterface
 
     public void PrintField(byte[,] matrix)
     {
-        Console.Write("    ");
-        for (byte column = 0; column < matrix.GetLongLength(1); column++)
-        {
-            Console.Write(column + " ");
-        }
+        //Console.Write("    ");
+        //for (byte column = 0; column < matrix.GetLongLength(1); column++)
+        //{
+        //    Console.Write(column + " ");
+        //}
 
-        Console.Write(Environment.NewLine + "   " + new string('-', matrix.GetLength(1) * 2 + 1) + Environment.NewLine);
+        Console.WriteLine(COLUMN_INDECES);
+
+        Console.WriteLine("   " + this.GetDashedLine(1 + matrix.GetLength(1) * 2));
 
         for (byte i = 0; i < matrix.GetLongLength(0); i++)
         {
-            Console.Write(i + " | ");
+            Console.Write(i + " |");
             for (byte j = 0; j < matrix.GetLongLength(1); j++)
             {
                 if (matrix[i, j] == 0)
@@ -30,13 +47,16 @@ public class ConsoleUI : IBaloonsUserInterface
                     continue;
                 }
 
-                Console.Write(matrix[i, j] + " ");
+                this.SetConsoleColor(matrix[i, j]);
+                Console.Write("{0, 2}", matrix[i, j]);
+                this.SetConsoleColorToDefault();
             }
 
-            Console.WriteLine("| ");
+            Console.WriteLine("{0, 2}", '|');
         }
 
-        Console.Write("   " + new string('-', matrix.GetLength(1) * 2 + 1) + Environment.NewLine);
+
+        Console.WriteLine("   " + this.GetDashedLine(1 + matrix.GetLength(1) * 2));
 
     }
 
@@ -49,5 +69,22 @@ public class ConsoleUI : IBaloonsUserInterface
     public string ReadUserInput()
     {
         return Console.ReadLine();
+    }
+
+    private string GetDashedLine(int count)
+    {
+        var dashedLine = new string('-', count);
+
+        return dashedLine;
+    }
+
+    private void SetConsoleColor(int key)
+    {
+        Console.ForegroundColor = this.colorsByNumbers[key];
+    }
+
+    private void SetConsoleColorToDefault()
+    {
+        Console.ForegroundColor = DEFAULT_COLOR;
     }
 }
