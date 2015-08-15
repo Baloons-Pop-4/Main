258643d59e9b8215a1339b2f91c3a665f4fd1c15
KonstantinSimeonov <kon.simeonov@gmail.com>
2015-08-15 10:09:27 +0300

    Extracted the game class in another .cs file in the same project

--- src/Engine/Engine.cs
+++ src/Engine/Engine.cs
@@ -3,45 +3,6 @@
     using System;
     using Contracts;
 
-    internal class Game
-    {
-        private byte[,] field;
-
-        private int userMovesCount;
-
-        public Game()
-        {
-            this.Reset();
-        }
-
-        public byte[,] Field
-        {
-            get
-            {
-                return this.field;
-            }
-        }
-
-        public int UserMovesCount
-        {
-            get
-            {
-                return this.userMovesCount;
-            }
-        }
-
-        public void Reset()
-        {
-            this.field = GameLogic.GenerateField();
-            this.userMovesCount = 0;
-        }
-
-        public void IncrementMoves()
-        {
-            this.userMovesCount++;
-        }
-    }
-
     public class Engine : IEngine
     {
         #region Constants
