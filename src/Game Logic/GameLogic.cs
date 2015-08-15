95eb691b49168a5c000c7da06035299e5e5de2df
KonstantinSimeonov <kon.simeonov@gmail.com>
2015-08-15 10:54:07 +0300

    Removed the unused printMatrix method from the GameLogic class

--- src/Game Logic/GameLogic.cs
+++ src/Game Logic/GameLogic.cs
@@ -33,54 +33,6 @@
             return newField;
         }
 
-        public static void printMatrix(byte[,] matrix)
-        {
-            Console.Write("    ");
-            for (byte column = 0; column < matrix.GetLongLength(1); column++)
-            {
-                Console.Write(column + " ");
-            }
-
-
-            Console.Write("\n   ");
-            for (byte column = 0; column < matrix.GetLongLength(1) * 2 + 1; column++)
-            {
-                Console.Write("-");
-
-
-
-            }
-
-            Console.WriteLine();
-
-            for (byte i = 0; i < matrix.GetLongLength(0); i++)
-            {
-                Console.Write(i + " | ");
-                for (byte j = 0; j < matrix.GetLongLength(1); j++)
-                {
-                    if (matrix[i, j] == 0)
-                    {
-                        Console.Write("  ");
-                        continue;
-                    }
-
-
-
-                    Console.Write(matrix[i, j] + " ");
-                }
-                Console.Write("| ");
-                Console.WriteLine();
-            }
-
-            Console.Write("   ");
-            for (byte column = 0; column < matrix.GetLongLength(1) * 2 + 1; column++)
-            {
-                Console.Write("-");
-            }
-            Console.WriteLine();
-
-        }
-
         public static void checkLeft(byte[,] matrix, int row, int column, int searchedItem)
         {
             int newRow = row;
