0a6b6a60c0376ad079b87bfa44450d5ec1e7d395
KonstantinSimeonov <kon.simeonov@gmail.com>
2015-08-17 16:44:52 +0300

    Starting implementing PopBaloon command
    Need to refactor the switch case in the Engine.Run method

---
+++ src/BaloonsPop.Engine/Commands/PopBaloonCommand.cs
@@ -0,0 +1,53 @@
+﻿namespace BaloonsPop.Engine.Commands
+{
+    using Common.Validators;
+
+    public class PopBaloonCommand : GameCommand
+    {
+        private GameLogic gameLogicProvider;
+
+        private IUserInputValidator validator;
+
+        private string rawCommand;
+
+        private string parsedCommand;
+
+        private int row;
+
+        private int col;
+
+        private bool inputIsValid;
+
+        public PopBaloonCommand(Game gameModel, GameLogic gameLogicProvider, IUserInputValidator validator, string commandLine) 
+            : base(gameModel)
+        {
+            this.gameLogicProvider = gameLogicProvider;
+            this.validator = validator;
+        }
+
+        public override void Execute()
+        {
+            if(!this.inputIsValid)
+            {
+                return;
+            }
+
+        }
+
+        private void TryParseUserInput()
+        {
+            if(!validator.IsValidUserMove(rawCommand))
+            {
+                this.inputIsValid = false;
+
+                return;
+            }
+
+            bool rowParsedSuccessfully = int.TryParse(this.rawCommand[0].ToString(), out this.row);
+
+            bool colParsedSuccessfully = int.TryParse(this.rawCommand[2].ToString(), out this.col);
+
+            this.inputIsValid = rowParsedSuccessfully && colParsedSuccessfully;
+        }
+    }
+}
