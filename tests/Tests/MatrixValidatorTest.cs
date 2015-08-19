752015dc5c1bae1b514794333d255948d2ffe7bd
KonstantinSimeonov <kon.simeonov@gmail.com>
2015-08-19 15:24:16 +0300

    Added MatrixValidator tests

---
+++ tests/Tests/MatrixValidatorTest.cs
@@ -0,0 +1,37 @@
+﻿namespace Tests
+{
+    using System;
+    using BaloonsPop.Common.Validators;
+    using BaloonsPop.Engine;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class MatrixValidatorTests
+    {
+        private MatrixValidator validator = MatrixValidator.GetInstance;
+
+        [TestMethod]
+        public void TestIfIsInsideMethodReturnsFalseForTooSmallRow()
+        {
+            Assert.IsFalse(validator.IsInsideMatrix(new byte[2, 5], -1, 2));
+        }
+
+        [TestMethod]
+        public void TestIfInsideMethodReturnsFalseForTooLargeRow()
+        {
+            Assert.IsFalse(validator.IsInsideMatrix(new byte[2, 5], 2, 2));
+        }
+
+        [TestMethod]
+        public void TestIfInsideMethodReturnsFalseForTooSmallCol()
+        {
+            Assert.IsFalse(validator.IsInsideMatrix(new byte[2, 5], 2, -1));
+        }
+
+        [TestMethod]
+        public void TestIfInsideMethodReturnsFalseForTooLargeCol()
+        {
+            Assert.IsFalse(validator.IsInsideMatrix(new byte[2, 5], 0, 5));
+        }
+    }
+}
