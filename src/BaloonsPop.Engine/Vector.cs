3d9b7175d815b5ce00ef957f2fc1178d50ea47f6
KonstantinSimeonov <kon.simeonov@gmail.com>
2015-08-22 18:40:43 +0300

    Added an immutable vector for popping directions

---
+++ src/BaloonsPop.Engine/Vector.cs
@@ -0,0 +1,38 @@
+﻿namespace BaloonsPop.Engine
+{
+    public struct Vector
+    {
+        private const int DIMENSION = 2;
+
+        private int[] coordinates;
+
+        public Vector(int x, int y)
+        {
+            this.coordinates = new int[DIMENSION];
+            this.coordinates[0] = x;
+            this.coordinates[1] = y;
+        }
+
+        public int X
+        {
+            get
+            {
+                return this.coordinates[0];
+            }
+        }
+
+        public int Y
+        {
+            get
+            {
+                return this.coordinates[1];
+            }
+        }
+
+        public override Vector operator+ (Vector a, Vector b)
+        {
+            var result = new Vector(a.X + b.X, a.Y + b.Y);
+            return result;
+        }
+    }
+}
