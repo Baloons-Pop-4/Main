7a8722635eac86633c459a548ba0bda16f2c717e
KonstantinSimeonov <kon.simeonov@gmail.com>
2015-09-16 20:47:22 +0300

    3 command factory tests

---
+++ tests/Tests/CommandTests.cs
@@ -0,0 +1,54 @@
+﻿namespace Tests
+{
+    using System;
+    using BalloonsPop.Common.Contracts;
+    using BalloonsPop.Engine.Commands;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class CommandTests
+    {
+        private ICommandFactory commandFactory;
+
+        public CommandTests()
+        {
+            this.commandFactory = new CommandFactory();
+        }
+
+        [TestMethod]
+        public void TestIfNonNullObjectIsReturnedWithValidCommandKey()
+        {
+            var commands = new string[] {"message", "exit", "undo", "pop", "restart", "field", "save", "top" };
+
+            foreach (var key in commands)
+            {
+                var cmd = this.commandFactory.CreateCommand(key);
+
+                Assert.AreNotEqual(null, cmd);
+            }
+        }
+
+        [TestMethod]
+        public void TestIfAnExceptionIsThrowWithInvalidCommandKeys()
+        {
+            var invalidKeys = new string[] { "jksdfjds", "redo", "quit", "pop_all", "cheat", "throw", "" };
+
+            foreach (var key in invalidKeys)
+            {
+                try
+                {
+                    this.commandFactory.CreateCommand(key);
+                }
+                catch(ArgumentException)
+                {
+                    continue;
+                }
+
+                Assert.Fail();
+            }
+        }
+
+
+    }
+}
