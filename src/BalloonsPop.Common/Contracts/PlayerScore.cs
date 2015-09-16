7ce7bc4a4e3df4121ade95dac19fcefcb1739552
r1f <ivaylo.arnaudov12@gmail.com>
2015-09-17 02:12:36 +0300

    Improved and moved the PlayerScore struct

---
+++ src/BalloonsPop.Common/Contracts/PlayerScore.cs
@@ -0,0 +1,23 @@
+﻿namespace BalloonsPop.Common.Contracts
+{
+    using System;
+
+    public struct PlayerScore : IComparable<PlayerScore>
+    {
+        public string Name;
+        public int Moves;
+        public DateTime Time;
+
+        public PlayerScore(string name, int moves, DateTime time)
+        {
+            this.Name = name;
+            this.Moves = moves;
+            this.Time = time;
+        }
+
+        public int CompareTo(PlayerScore other)
+        {
+            return this.Moves.CompareTo(other.Moves);
+        }
+    }
+}
