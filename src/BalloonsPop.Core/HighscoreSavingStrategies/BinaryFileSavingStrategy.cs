5f8dad71a7c4d0b81a8b402668cbf6133c0a1d7b
r1f <ivaylo.arnaudov12@gmail.com>
2015-09-23 22:57:55 +0300

    Implemented an XML and binary highscore saving strategies

---
+++ src/BalloonsPop.Core/HighscoreSavingStrategies/BinaryFileSavingStrategy.cs
@@ -0,0 +1,20 @@
+﻿namespace BalloonsPop.Core
+{
+    using BalloonsPop.Common.Contracts;
+    using System.Collections.Generic;
+    using System.IO;
+
+    public class BinaryFileSavingStrategy : IHighscoreSavingStrategy
+    {
+        private const string FilePath = "highscore.bin";
+
+        public void Save(List<PlayerScore> highscoreTable)
+        {
+            using (Stream stream = File.Open(FilePath, FileMode.Create))
+            {
+                var binaryFormatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+                binaryFormatter.Serialize(stream, highscoreTable);
+            }
+        }
+    }
+}
