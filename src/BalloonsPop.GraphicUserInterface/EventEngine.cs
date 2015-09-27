abc24e970dc5253496b4854bfdb4241fafb8f5d2
KonstantinSimeonov <kon.simeonov@gmail.com>
2015-09-27 21:15:49 +0300

    Deleted the evil constructor

--- src/BalloonsPop.GraphicUserInterface/EventEngine.cs
+++ src/BalloonsPop.GraphicUserInterface/EventEngine.cs
@@ -2,9 +2,7 @@
 {
     using System;
 
-    using BalloonsPop.Common.Contracts;
     using BalloonsPop.Core;
-    using System.Collections.Generic;
 
     public class EventEngine : EngineCore, IEventEngine
     {
@@ -14,20 +12,6 @@
             dependencyBundle.Gui.Raise += new EventHandler(this.HandleUserInput);
             this.context.Game.Field = this.context.LogicProvider.GenerateField();    
         }
-
-        //protected EventEngine(
-        //    IEventBasedUserInterface ui,
-        //    IUserInputValidator validator,
-        //    ICommandFactory commandFactory,
-        //    IGameModel gameModel,
-        //    IGameLogicProvider gameLogicProvider,
-        //    IHighscoreTable scoreTable,
-        //    IHighscoreSaver saver
-        //    )
-        //    :base(ui, validator, scoreTable, saver, commandFactory, gameModel, gameLogicProvider)
-        //{
-        //    //ui.Raise += new EventHandler(this.HandleUserInput);
-        //}
 
         public void HandleUserInput(object sender, EventArgs e)
         {
