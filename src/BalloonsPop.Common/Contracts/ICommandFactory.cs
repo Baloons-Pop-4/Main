0649c09efe1803885f74a99728806cfec50dc65d
KonstantinSimeonov <kon.simeonov@gmail.com>
2015-09-30 05:32:29 +0300

    Introduced extendable factory for commands

--- src/BalloonsPop.Common/Contracts/ICommandFactory.cs
+++ src/BalloonsPop.Common/Contracts/ICommandFactory.cs
@@ -1,7 +1,11 @@
 ﻿namespace BalloonsPop.Common.Contracts
 {
+    using System;
+
     public interface ICommandFactory
     {
         ICommand CreateCommand(string commandName);
+        void RegisterCommand(string commandKey, Func<ICommand> commandProvidingMethod);
+        void UnregisterCommand(string commandKey);
     }
 }
