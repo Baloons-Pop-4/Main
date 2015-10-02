c660e45a08fd539539d2a13cb0c8ab0e4855f82f
KonstantinSimeonov <kon.simeonov@gmail.com>
2015-10-02 17:09:23 +0300

    Tests for IsMatch method of ReflectionCloning class

---
+++ tests/Tests/CloningStrategiesTests/ReflectionCloningTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests.CloningStrategiesTests
+{
+    using BalloonsPop.Core.Memento.CloningStrategies;
+
+    [TestClass]
+    public class ReflectionCloningTests
+    {
+        internal class ClasWithEmptyConstructor
+        {
+            public ClasWithEmptyConstructor()
+            {
+
+            }
+        }
+
+        internal class ClassWithNoEmptyConstructor
+        {
+            public ClassWithNoEmptyConstructor(bool someVariable)
+            {}
+        }
+
+        ReflectionCloning<ClasWithEmptyConstructor> validReflectionCloning;
+        ReflectionCloning<ClassWithNoEmptyConstructor> invalidReflectionCloning;
+
+        ClasWithEmptyConstructor validTestObj;
+        ClassWithNoEmptyConstructor invalidTestObj;
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            this.validReflectionCloning = new ReflectionCloning<ClasWithEmptyConstructor>();
+            this.invalidReflectionCloning = new ReflectionCloning<ClassWithNoEmptyConstructor>();
+
+            this.validTestObj = new ClasWithEmptyConstructor();
+            this.invalidTestObj = new ClassWithNoEmptyConstructor(true);
+        }
+
+        [TestMethod]
+        public void TestIfIsMatchReturnsTrueForTypesWithEmptyConstructor()
+        {
+            Assert.IsTrue(this.validReflectionCloning.IsMatch(this.validTestObj));
+        }
+
+        [TestMethod]
+        public void TestIfIsMatchReturnsFalseForTypesWithNoEmptyConstructor()
+        {
+            Assert.IsFalse(this.invalidReflectionCloning.IsMatch(this.invalidTestObj));
+        }
+    }
+}
