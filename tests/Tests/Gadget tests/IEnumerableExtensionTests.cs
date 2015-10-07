68626576a3671054da6b143f30ef73820e5b2b1a
juvemar <martoo.atanasov@gmail.com>
2015-10-07 15:00:55 +0300

    Added little tiny tests to BalloonsPop.Common/Gadgets: StringExtensions.cs and IEnumerableExtensionTests.cs

---
+++ tests/Tests/Gadget tests/IEnumerableExtensionTests.cs
@@ -0,0 +1,37 @@
+﻿namespace Tests.Gadget_tests
+{
+    using System;
+    using System.Collections.Generic;
+
+    using BalloonsPop.Common.Gadgets;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class IEnumerableExtensionTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException),
+        "Cannot foreach null collection.")]
+        public void ForeachOfNullCollectionShouldThrowNullReferenceException()
+        {
+            IEnumerable<byte> collection = new List<byte>();
+            collection = null;
+            Action<byte> action = null;
+
+            IEnumerableExtension.ForEach<byte>(collection, action);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException),
+        "Cannot execute null method on a collection.")]
+        public void ExecutingNullMethodOnCollectionShouldThrowNullReferenceException()
+        {
+            IEnumerable<byte> collection = new List<byte>();
+            collection = null;
+            Action<byte> action = null;
+
+            IEnumerableExtension.ForEach<byte>(collection, action);
+        }
+    }
+}
---
+++ tests/Tests/Gadget tests/StringExtensionsTests.cs
@@ -0,0 +1,46 @@
+﻿namespace Tests.Gadget_tests
+{
+    using System;
+
+    using BalloonsPop.Common.Gadgets;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class StringExtensionsTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException),
+        "A string could not be parsed to Int32.")]
+        public void ParsingWordsShouldThrowArgumentException()
+        {
+            int result = StringExtensions.ToInt32("Thirty one");
+        }
+
+        [TestMethod]
+        public void ParsingStringNumberToInt32ShouldWorkCorrectly()
+        {
+            int result = StringExtensions.ToInt32("31");
+            int expectedResult = 31;
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException),
+            "A char could not be parsed to Int32")]
+        public void ParsingLetterShouldThrowArgumentException()
+        {
+            int result = StringExtensions.ToInt32('a');
+        }
+
+        [TestMethod]
+        public void ParsingDigitToInt32ShouldWorkCorrectly()
+        {
+            int result = StringExtensions.ToInt32('3');
+            int expectedResult = 3;
+
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
