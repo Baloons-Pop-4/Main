9065bc10d51fb307a016dd7442d817a098cd2461
KonstantinSimeonov <kon.simeonov@gmail.com>
2015-10-09 21:28:09 +0300

    Controller tests -_^

--- tests/Tests/WpfUiTests/MainWindowControllerTests.cs
+++ tests/Tests/WpfUiTests/MainWindowControllerTests.cs
@@ -2,6 +2,7 @@
 using System.Text;
 using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BalloonsPop.GraphicUserInterface;
 
 namespace Tests.WpfUiTests
 {
@@ -11,59 +12,52 @@ namespace Tests.WpfUiTests
     [TestClass]
     public class MainWindowControllerTests
     {
-        public MainWindowControllerTests()
+        private BalloonsView view;
+        private MainWindowController controller;
+        private Resources resourceProvider;
+
+        [TestInitialize]
+        public void TestInit()
         {
-            //
-            // TODO: Add constructor logic here
-            //
+            this.view = new BalloonsView();
+            this.resourceProvider = new Resources();
+            this.controller = new MainWindowController(this.view, this.resourceProvider);
         }
-
-        private TestContext testContextInstance;
-
-        /// <summary>
-        ///Gets or sets the test context which provides
-        ///information about and functionality for the current test run.
-        ///</summary>
-        public TestContext TestContext
-        {
-            get
-            {
-                return testContextInstance;
-            }
-            set
-            {
-                testContextInstance = value;
-            }
-        }
-
-        #region Additional test attributes
-        //
-        // You can use the following additional attributes as you write your tests:
-        //
-        // Use ClassInitialize to run code before running the first test in the class
-        // [ClassInitialize()]
-        // public static void MyClassInitialize(TestContext testContext) { }
-        //
-        // Use ClassCleanup to run code after all tests in a class have run
-        // [ClassCleanup()]
-        // public static void MyClassCleanup() { }
-        //
-        // Use TestInitialize to run code before running each test 
-        // [TestInitialize()]
-        // public void MyTestInitialize() { }
-        //
-        // Use TestCleanup to run code after each test has run
-        // [TestCleanup()]
-        // public void MyTestCleanup() { }
-        //
-        #endregion
 
         [TestMethod]
-        public void TestMethod1()
+        public void TestIfControllersRaisePropertyAddsMethodsFromBalloonsViewRaiseCommandEventHandler()
         {
-            //
-            // TODO: Add test logic here
-            //
+            bool hasBeenCalled = false;
+            this.controller.RaiseCommand += (s, e) => hasBeenCalled = true;
+            this.view.Raise(view, new EventArgs());
+
+            Assert.IsTrue(hasBeenCalled);
+        }
+
+        [TestMethod]
+        public void TestIfControllerPropertyRaiseCommandRemovesMethodsFromViewsRaiseCommandEventHandler()
+        {
+            bool hasBeenCalled = false;
+            EventHandler action = (s, e) => hasBeenCalled = true;
+            this.controller.RaiseCommand += (s, e) => { };
+            this.controller.RaiseCommand += action;
+            this.controller.RaiseCommand -= action;
+            this.view.Raise(view, new EventArgs());
+
+            Assert.IsFalse(hasBeenCalled);
+        }
+
+        [TestMethod]
+        public void TestIfPrintMessageSetsTheViewMessageProperty()
+        {
+            this.controller.PrintMessage("peshoq");
+            Assert.AreEqual("peshoq", this.view.Message);
+        }
+
+        [TestMethod]
+        public void TestIfTheWindowPropertyOfTheControllerReturnsTheSameViewItHasBeenPassedInConstructor()
+        {
+            Assert.AreSame(this.view, this.controller.Window);
         }
     }
 }
