fee27f5c438bae7bf16c3db1f9e20e6d410b3b11
juvemar <martoo.atanasov@gmail.com>
2015-10-09 09:46:31 +0300

    Added more tests for IEnumerableExtension.cs, covered 100%

--- tests/Tests/Gadget tests/IEnumerableExtensionTests.cs
+++ tests/Tests/Gadget tests/IEnumerableExtensionTests.cs
@@ -25,13 +25,40 @@
         [TestMethod]
         [ExpectedException(typeof(NullReferenceException),
         "Cannot execute null method on a collection.")]
-        public void ExecutingNullMethodOnCollectionShouldThrowNullReferenceException()
+        public void ExecutingNullActionOnCollectionShouldThrowNullReferenceException()
         {
-            IEnumerable<byte> collection = new List<byte>();
-            collection = null;
+            IEnumerable<byte> collection = new List<byte> { 1, 2, 3, 4 };
             Action<byte> action = null;
 
             IEnumerableExtension.ForEach<byte>(collection, action);
+        }
+
+        [TestMethod]
+        public void ExecutingNotNullActionOnCollectionShouldNotBeNull()
+        {
+            IEnumerable<byte> collection = new List<byte> { 1, 2, 3, 4, 5, 6 };
+            Action<byte> action = x => x++;
+
+            IEnumerableExtension.ForEach<byte>(collection, action);
+
+            Assert.IsNotNull(collection);
+        }
+
+        [TestMethod]
+        public void ExecutingActionOnCollectionShouldWorkProperly()
+        {
+            var collection = new List<byte> { 1, 2, 3, 4, 5, 6 };
+            var collection2 = new List<byte>();
+            Action<byte> action = x => collection2.Add(x);
+
+            IEnumerableExtension.ForEach<byte>(collection, action);
+
+            Assert.AreEqual(collection.Count, collection2.Count);
+
+            for (int i = 0, length = collection.Count; i < length; i++)
+            {
+                Assert.AreEqual(collection[i], collection2[i]);
+            }
         }
     }
 }
