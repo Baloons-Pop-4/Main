9cdb170bf814357e517bb3aba714cf5ee31a7bd1
juvemar <martoo.atanasov@gmail.com>
2015-10-09 10:46:15 +0300

    Implemented SoundsLoader.cs and SoundsPlayer.cs

---
+++ src/BalloonsPop.SoundPlayer/SoundsLoader.cs
@@ -0,0 +1,22 @@
+﻿namespace BalloonsPop.SoundPlayer
+{
+    using System.Media;
+
+    using BalloonsPop.SoundPlayer.Contracts;
+
+    public class SoundsLoader : ISoundsLoader
+    {
+        public SoundPlayer CreateSoundMedia(string soundName)
+        {
+            string path = BuildFilePath(soundName);
+            var player = new SoundPlayer(path);
+            return player;
+        }
+
+        private string BuildFilePath(string soundName)
+        {
+            string filePath = "../../Sounds/" + soundName + ".wav";
+            return filePath;
+        }
+    }
+}
---
+++ src/BalloonsPop.SoundPlayer/SoundsPlayer.cs
@@ -0,0 +1,37 @@
+﻿namespace BalloonsPop.SoundPlayer
+{
+    using System.Collections.Generic;
+    using System.Media;
+
+    using BalloonsPop.SoundPlayer.Contracts;
+
+    public class SoundsPlayer : ISoundsPlayer
+    {
+        private IDictionary<string, ISound> sounds;
+        private ISoundsLoader loader;
+
+        public SoundsPlayer(ISoundsLoader loader)
+        {
+            this.sounds = new Dictionary<string, ISound>();
+            this.loader = loader;
+        }
+
+        public void PlaySound(string soundName)
+        {
+            if (!this.sounds.ContainsKey(soundName))
+            {
+                RegisterSound(soundName);
+            }
+
+            SoundPlayer player = loader.CreateSoundMedia(soundName);
+            player.Play();
+
+        }
+
+        public void RegisterSound(string soundName)
+        {
+            ISound newSound = new Sound(soundName);
+            this.sounds.Add(soundName, newSound);
+        }
+    }
+}
