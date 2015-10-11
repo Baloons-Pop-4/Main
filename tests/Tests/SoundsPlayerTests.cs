ae3374220ae6f5cf36f5f1dc7a42b580239b49dd
juvemar <martoo.atanasov@gmail.com>
2015-10-11 20:40:16 +0300

    Added SoundsPlayerTests.cs

---
+++ tests/Tests/SoundsPlayerTests.cs
@@ -0,0 +1,23 @@
+﻿namespace Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.Media;
+
+    using BalloonsPop.SoundPlayer;
+
+    [TestClass]
+    public class SoundsPlayerTests
+    {
+        [TestMethod]
+        public void SoundsShouldBeRegisteredProperlyToSoundsList()
+        {
+            SoundPlayer playerMedia = new SoundPlayer();
+            SoundsLoader loader = new SoundsLoader();
+            SoundsPlayer player = new SoundsPlayer(loader);
+
+            player.RegisterSound("win", playerMedia);
+
+            Assert.AreEqual(1, player.CountSounds());
+        }
+    }
+}
